f82eb46b2532603eb103bb8251c1d0b1f0a30b45
mzyan <mengzhao.yan@foxmail.com>
2020-01-12 22:43:49 +0800

    博客编辑删除功能

--- src/main/java/com/mzyan/blog/web/admin/BlogController.java
+++ src/main/java/com/mzyan/blog/web/admin/BlogController.java
@@ -13,11 +13,13 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
+import java.util.Date;
 
 /**
  * Created by mzYan on 2020-01-11 15:18
@@ -29,6 +31,9 @@ public class BlogController {
     private static final String LIST = "admin/blogs";
     private static final String REDIRECT_LIST = "redirect:/admin/blogs";
     private static final String INPUT = "admin/blogs-input";
+
+    private Date createTime;
+    private Date updateTime;
 
 
     @Autowired
@@ -51,26 +56,50 @@ public class BlogController {
         return "admin/blogs :: bloglist";
     }
 
-    @GetMapping("/blogs/input")
-    public String input(Model model) {
+    private void setTypeAndTag(Model model) {
         model.addAttribute("types", typeService.listType());
         model.addAttribute("tags", tagService.listTag());
+    }
+
+    @GetMapping("/blogs/input")
+    public String input(Model model) {
+        setTypeAndTag(model);
         model.addAttribute("blog", new Blog());
         return INPUT;
     }
 
+    @GetMapping("/blogs/{id}/input")
+    public String editInput(@PathVariable Long id, Model model) {
+        setTypeAndTag(model);
+        Blog blog = blogService.getBlog(id);
+        createTime = blog.getCreateTime();
+        updateTime = blog.getUpdateTime();
+        blog.init();
+        model.addAttribute("blog", blog);
+        return INPUT;
+    }
+
+    // 新增与修改
     @PostMapping("/blogs")
     public String post(Blog blog, RedirectAttributes attributes, HttpSession session) {
         blog.setUser((User) session.getAttribute("user"));
-        // 感觉多此一举
         blog.setType(typeService.getType(blog.getType().getId()));
         blog.setTags(tagService.listTag(blog.getTagIds()));
+        blog.setCreateTime(createTime);
+        blog.setUpdateTime(updateTime);
         Blog b = blogService.saveBlog(blog);
         if (b == null) {
             attributes.addFlashAttribute("message", "操作失败");
         } else {
             attributes.addFlashAttribute("message", "操作成功");
         }
+        return REDIRECT_LIST;
+    }
+
+    @GetMapping("/blogs/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes attributes) {
+        blogService.deleteBlog(id);
+        attributes.addFlashAttribute("message", "删除成功");
         return REDIRECT_LIST;
     }
 }
