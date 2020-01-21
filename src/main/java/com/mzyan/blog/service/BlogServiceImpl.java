a80355fe43fc9767c6662beb16a24eb8721ad438
mzyan <mengzhao.yan@foxmail.com>
2020-01-21 19:22:48 +0800

    博客标签页面

--- src/main/java/com/mzyan/blog/service/BlogServiceImpl.java
+++ src/main/java/com/mzyan/blog/service/BlogServiceImpl.java
@@ -17,10 +17,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -55,6 +52,12 @@ public class BlogServiceImpl implements BlogService {
         return b;
     }
 
+    /**
+     * 模糊查询
+     * @param pageable
+     * @param blog
+     * @return
+     */
     @Transactional
     @Override
     public Page<Blog> listBlog(Pageable pageable, BlogQuery blog) {
@@ -81,6 +84,24 @@ public class BlogServiceImpl implements BlogService {
     @Override
     public Page<Blog> listBlog(Pageable pageable) {
         return blogRepository.findAll(pageable);
+    }
+
+    /**
+     * 关联查询
+     * @param tagId
+     * @param pageable
+     * @return
+     */
+    @Transactional
+    @Override
+    public Page<Blog> listBlog(Long tagId, Pageable pageable) {
+        return blogRepository.findAll(new Specification<Blog>() {
+            @Override
+            public Predicate toPredicate(Root<Blog> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
+                Join join = root.join("tags");
+                return cb.equal(join.get("id"), tagId);
+            }
+        }, pageable);
     }
 
     @Transactional
